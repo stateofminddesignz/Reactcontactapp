14d2cf215693b8f8eeffc2711683761fe568e77d
Larry McCullough <98300716+stateofminddesignz@users.noreply.github.com>
2023-05-05 18:39:02 -0400

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "../components/Card.jsx";
+import Header from "../components/Header.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Card />
+    </>
+  );
+}
+
+export default App;
